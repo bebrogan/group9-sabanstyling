2cf7965a30954681a6246a573e29430bbf8b043f
Bailey Bowers <you@example.com>
2021-11-18 14:18:55 -0600

    another controller

---
+++ api/Data/PaymentDataHandler.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using api.Interfaces;
+using api.Models;
+
+namespace api.Data
+{
+    public class PaymentDataHandler : IPaymentDataHandler
+    {
+        public void Delete(Payment pay)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Insert(Payment pay)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Payment> Select()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(Payment pay)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
