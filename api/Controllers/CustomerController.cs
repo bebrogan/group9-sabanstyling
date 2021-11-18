e8bc5e87af7689bd3710f0ae210d8fa856135ae1
Bailey Bowers <you@example.com>
2021-11-18 13:23:47 -0600

    update cors

--- api/Controllers/CustomerController.cs
+++ api/Controllers/CustomerController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
 
 namespace api.Controllers
 {
@@ -12,6 +13,7 @@ namespace api.Controllers
     public class CustomerController : ControllerBase
     {
         // GET: api/Customer
+        [EnableCors("OpenPolicy")]
         [HttpGet]
         public IEnumerable<string> Get()
         {
@@ -19,6 +21,7 @@ namespace api.Controllers
         }
 
         // GET: api/Customer/5
+        [EnableCors("OpenPolicy")]
         [HttpGet("{id}", Name = "Get")]
         public string Get(int id)
         {
@@ -26,18 +29,21 @@ namespace api.Controllers
         }
 
         // POST: api/Customer
+        [EnableCors("OpenPolicy")]
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
         // PUT: api/Customer/5
+        [EnableCors("OpenPolicy")]
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
         // DELETE: api/Customer/5
+        [EnableCors("OpenPolicy")]
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
--- api/Startup.cs
+++ api/Startup.cs
@@ -26,6 +26,17 @@ namespace api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+             options.AddPolicy("AnotherPolicy",
+                 builder =>
+                 {
+                    builder.AllowAnyOrigin()
+                               .AllowAnyHeader()
+                               .AllowAnyMethod();
+                 });
+        }); 
+
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -47,6 +58,8 @@ namespace api
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors();
 
             app.UseAuthorization();
 
