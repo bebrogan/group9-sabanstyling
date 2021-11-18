8dcaefb0b5b62d692744d06200bb3d712d22ee42
Bailey Bowers <you@example.com>
2021-11-18 13:57:34 -0600

    clothing and employee

---
+++ api/Models/Clothing.cs
@@ -0,0 +1,11 @@
+namespace api.Models
+{
+    public class Clothing
+    {
+        public int ID {get; set;}
+        public string Size {get; set;}
+        public string Type {get; set;}
+        public string Link {get; set;}
+        public double Price {get; set;}
+    }
+}
