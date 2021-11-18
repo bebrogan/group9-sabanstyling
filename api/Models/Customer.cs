39220f0952ff88fe5ead96a2c830ba4a5b212fca
Bailey Bowers <you@example.com>
2021-11-18 13:48:11 -0600

    customer vars

--- api/Models/Customer.cs
+++ api/Models/Customer.cs
@@ -1,8 +1,16 @@
+using System;
+
 namespace api.Models
 {
     public class Customer
     {
+        public int ID {get; set;}
+        public string Email {get; set;}
         public string FirstName{ get; set;}
         public string LastName{get; set;}
+        public int Phone {get; set;}
+        public string Why {get; set;}
+        public DateTime Date {get; set;}
+
     }
 }
