f488b79bc9c64c9d36eaa8c7fe2206de61d80040
Bailey Bowers <you@example.com>
2021-11-29 11:20:33 -0600

    employeedatahandler

--- api/Data/EmployeeDataHandler.cs
+++ api/Data/EmployeeDataHandler.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Dynamic;
 using api.Interfaces;
 using api.Models;
 
@@ -18,17 +19,86 @@ namespace api.Data
 
         public void Insert(Employee emp)
         {
-            throw new System.NotImplementedException();
+            string sql = "INSERT INTO employee (empID, empfName, emplName, empEmail, empPassword) VALUES(@id, @firstName, @lastName, @email, @password);"; 
+
+            var values = GetValues(emp); 
+            db.Open(); 
+            db.Insert(sql, values);
+            db.Close();
         }
 
         public List<Employee> Select()
         {
-            throw new System.NotImplementedException();
+            db.Open(); 
+
+            string sql = "SELECT * from employee ORDER BY 'id' desc"; 
+
+            List<ExpandoObject> results = db.Select(sql); 
+
+            List<Employee> emp = new List<Employee>(); 
+            foreach(dynamic item in results)
+            {
+                Employee temp = new Employee()
+                {
+                    ID = item.id,
+                    FirstName = item.empfName,
+                    LastName = item.emplName,
+                    Email = item.empEmail,
+                    Password = item.password,
+                }; 
+                emp.Add(temp); 
+            }
+
+            db.Close(); 
+            return emp; 
         }
 
         public void Update(Employee emp)
         {
-            throw new System.NotImplementedException();
+            string sql = "UPDATE employee SET ";
+
+            var values = GetValues(emp); 
+            foreach(var p in values)
+            {
+                if(p.Key != "ID" && p.Value != null)   
+                {
+                    switch(p.Key)
+                    {
+                        case "@message":
+                            sql += "post = @message,";
+                            break; 
+                        case "@firstName":
+                            sql += "empfName = @firstName,";
+                            break; 
+                        case "@lastName":
+                            sql += "emplName = @lastName,";
+                            break; 
+                        case "@email":
+                            sql += "empEmail = @email,";
+                            break; 
+                        case "@password":
+                            sql += "empPassword = @password,";
+                            break; 
+                    }
+                }
+            }
+            sql = sql.Remove(sql.Length - 1, 1);
+            sql += " WHERE id=@id"; 
+            db.Open(); 
+            db.Update(sql, values);
+            db.Close();
+        }
+        public Dictionary<string, object> GetValues(Employee emp)
+        {
+            var values = new Dictionary<string,object>()
+            {
+                {"@id", emp.ID},
+                {"@firstName", emp.FirstName},
+                {"@lastName", emp.LastName},
+                {"@email", emp.Email},
+                {"@password", emp.Password},
+            }; 
+            return values; 
         }
     }
 }
