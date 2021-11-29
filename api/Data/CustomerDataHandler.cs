24b9733feb9e0e1fc7f3bafbe200223bc1f481a7
Brendan Brogan <brendanbrogan@Brendans-MacBook-Pro.local>
2021-11-29 11:17:28 -0600

    customerdatahandler

--- api/Data/CustomerDataHandler.cs
+++ api/Data/CustomerDataHandler.cs
@@ -13,22 +13,62 @@ namespace api.Data
         }
         public void Delete(Customer cust)
         {
-            throw new System.NotImplementedException();
+            string sql = "UPDATE customer SET deleted= 'Y' WHERE id=@ID";
+            var values = GetValues(post);
+            db.Open();
+            db.Update(sql, values);
+            db.Close();
         }
 
         public void Insert(Customer cust)
         {
-            throw new System.NotImplementedException();
+            string sql = "INSERT INTO customer (Id, Email, FirstName, LastName, Phone, Why, Date)";
+            sql += "VALUES (@ID, @Email, @FirstName, @LastName, @Phone, @Why, @Date)";
+
+            var values = GetValues(post);
+            db.Open();
+            db.Insert(sql, values);
+            db.Close();
         }
 
         public List<Customer> Select()
         {
-            throw new System.NotImplementedException();
+            List<Customer> myPost = new List<Customer>();
+
+            db.Open();
+            string sql = "SELECT * from customer";
+            
+            List<ExpandoObject> results = db.Select(sql);
+
+            foreach(dynamic item in results)
+            {
+                Post temp = new Post(){ID = item.id, 
+                Email = item.email,
+                FirstName = item.FirstName,
+                LastName = item.LastName,
+                Phone = item.phone,
+                Why = item.why,
+                Date = item.date};
+
+                myPost.Add(temp);
+
+
+            }
+            
+
+            // db.Close();
+
+            return myPost;
         }
 
         public void Update(Customer cust)
         {
-            throw new System.NotImplementedException();
+            string sql = "UPDATE customer SET id=@ID, email=@Email, why = @Why, phone = @Phone, WHERE id=@ID";
+
+            var values = GetValues(post);
+            db.Open();
+            db.Update(sql, values);
+            db.Close();
         }
     }
 }
