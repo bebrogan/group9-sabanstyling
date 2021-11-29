c1d7c66f13ad50849dca3e2e349741ee2c06c424
Brendan Brogan <brendanbrogan@Brendans-MacBook-Pro.local>
2021-11-29 11:32:45 -0600

    clothingdatahandler

--- api/Data/ClothingDataHandler.cs
+++ api/Data/ClothingDataHandler.cs
@@ -13,22 +13,58 @@ namespace api.Data
         }
         public void Delete(Clothing cloth)
         {
-            throw new System.NotImplementedException();
+            string sql = "UPDATE post SET deleted= 'Y' WHERE id=@Id";
+            var values = GetValues(cloth);
+            db.Open();
+            db.Update(sql, values);
+            db.Close();
         }
 
         public void Insert(Clothing cloth)
         {
-            throw new System.NotImplementedException();
+            string sql = "INSERT INTO customer (Id, Size, Type, Link, Price)";
+            sql += "VALUES (@ID, @Size, @Type, @Link, @Price)";
+
+            var values = GetValues(cloth);
+            db.Open();
+            db.Insert(sql, values);
+            db.Close();
         }
 
         public List<Clothing> Select()
         {
-            throw new System.NotImplementedException();
+            tList<Clothing> myClothing = new List<Clothing>();
+
+            db.Open();
+            string sql = "SELECT * from clothing";
+            
+            List<ExpandoObject> results = db.Select(sql);
+
+            foreach(dynamic item in results)
+            {
+                Post temp = new Post(){ID = item.id, Size = item.size,
+                Type = item.type,
+                Link = item.link,
+                Price = item.price};
+                myPost.Add(temp);
+
+
+            }
+            
+
+            // db.Close();
+
+            return myClothing;
         }
 
         public void Update(Clothing cloth)
         {
-            throw new System.NotImplementedException();
+            string sql = "UPDATE clothing SET id=@ID, size=@Size, type=@Type, link = @Link, price = @Price WHERE id=@ID";
+
+            var values = GetValues(post);
+            db.Open();
+            db.Update(sql, values);
+            db.Close();
         }
     }
 }
