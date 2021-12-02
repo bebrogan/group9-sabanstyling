524c6158f81bd83aa7e142f80388bf50120134f6
Bailey Bowers <you@example.com>
2021-12-02 14:18:45 -0600

    js

--- client/resources/scripts/index.js
+++ client/resources/scripts/index.js
@@ -1,16 +1,93 @@
 function handleOnLoad(){
-    const customerUrl = "https://localhost:5001/api/Customer"; 
+    const customerUrl = "https://localhost:5001/api/Clothing"; 
     console.log (customerUrl);
 
     fetch(customerUrl).then(function(response){
         return response.json();
     }).then(function(json){
-        console.log(json);
+        displayClothingTable(json);
     }).catch(function(error){
         console.log(error);
     });
 }
 
+function getCurrentUser(){
+    const str = localStorage.getItem("User");
+
+    // convert string to valid object
+    const userObj = JSON.parse(str);
+
+    console.log(userObj);
+    return userObj;
+}
+
+function userOnSubmit(){
+    var password = document.getElementById("password").value;
+    var email = document.getElementById("email").value;
+
+    getUserID(email, password);
+    window.location.href="../userdashboard.html;
+
+}
+
+function getUserID(username, password){
+    const customerUrl = "https://localhost:5001/api/Customer"; 
+    console.log (customerUrl);
+
+    
+
+    fetch(customerUrl).then(function(response){
+        return response.json();
+    }).then(function(json){
+        json.forEach(element=>
+            {
+                if((element.username == username)&&(element.password == password)){
+                    var user ;
+                }
+            });
+        
+    }).catch(function(error){
+        console.log(error);
+    });
+
+}
+
+function saveCurrentUser(user){
+    const userObj = JSON.stringify(user);
+    localStorage.setItem("User",userObj);
+}
+
+function displayClothingTable(json){
+    var currentUser = getCurrentUser();
+    let clothing = [];
+
+    json.forEach(element => {
+        if(element.userID == curentUser.id){
+            clothing.pop(element);
+        }
+    });
+    console.log(clothing);
+    
+    var dataTable = document.getElementById("dataTable");
+
+    var html = "<table id=\"table_id\" class=\"display\"><thead><tr><th>Item Name</th><th>Size</th><th>Link to Item</th></tr></thead><tbody>";
+    clothing.forEach(element => {
+        html += `<tr><td>${element.type}</td><td>${element.size}</td></tr><td>${element.link}</td></tr>`;
+    });
+    html+="</tbody></table>";
+    dataTable.innerHTML = html;
+    
+    
+    
+    
+    // var html = "<table id = \"postTable\" class =\"table table-success table-striped\" id=><tr><th>ID</th><th>Post</th></tr>";
+    // json.forEach(post => {
+    //     html += `<tr><td>${post.id}</td><td>${post.message}</td></tr>`;
+    // });
+    // html += "</table>";
+    // dataTable.innerHTML = html;
+    // console.log(json)
+}
 
 function handleOnSubmit(){
     var email = document.getElementById("email").value;
@@ -29,7 +106,7 @@ function handleOnSubmit(){
         Why : why,
         Password : password
     }
-
+    setCurrentUser(customer);
     PostCustomer(customer); 
 }
 
